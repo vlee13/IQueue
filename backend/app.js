8d4889fe3d16971346cab197527b7d14cac879e6
Tzikas <niko.tzikas@gmail.com>
2020-08-15 19:17:36 -0400

    deploy?

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://clientnetlify.netlify.app"] //Swap this with the client url 
+    origin: ["http://localhost:3000", "https://clientnetlify.netlify.app", "https://iqueue.netlify.app"] //Swap this with the client url 
   })
 );
 
